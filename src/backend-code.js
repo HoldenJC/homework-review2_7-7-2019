407768502eda4e98396488873839bb4f2aaeaab4
HoldenJC <holdenj_c@yahoo.com>
2019-07-07 23:40:35 -0700

    add workthrough of Promises hw

--- src/backend-code.js
+++ src/backend-code.js
@@ -1,10 +1,18 @@
-export class Example {
-  constructor(){
-    this.name = "Holden";
-  }
-
-  exampleMethod(){
-    this.name = "HoldenJC";
+export class WeatherService {
+  getWeatherByCity(city) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${process.env.exports.apiKey}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
   }
 }
 
--- src/main.js
+++ src/main.js
@@ -2,35 +2,109 @@ import $ from 'jquery';
 import './styles.css';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Example } from './backend-code';
-
-let example = new Example();
+import { WeatherService } from './backend-code';
 
 $(document).ready(function () {
-  $("#test").append(`<br>${example.name}`);
-  example.exampleMethod();
-  $("#test").append(`<br>${example.name}`);
-
+  
   $("#city").keyup(function (event) {
     if (event.keyCode === 13) {
 
-      $.ajax({
-        url: `http://api.openweathermap.org/data/2.5/weather?q=${$("#city").val()}&APPID=${process.env.exports.apiKey}`,
-        type: 'GET',
-        data: {
-          format: 'json'
-        },
-        success: function (response) {
-          $('#test').append(`<br>The humidity in ${response.name} is ${response.main.humidity}%`);
-          $('#test').append(`<br>The temperature in Kelvins is ${response.main.temp}.`);
-          $('#test').append(`<br>The current forecast is ${response.weather[0].main} (${response.weather[0].description}).`);
-          $('#city').val("");
-        },
-        error: function () {
-          $('#test').append("<br>There was an error processing your request. Please try again.");
-        }
+      /* API call with Javascript's XMLHttpRequest object and Javascripts Promise object with UI and backend logic separated ---------- */
+      let weatherService = new WeatherService();
+      let promise = weatherService.getWeatherByCity($("#city").val());
+
+      promise.then(function(response) {
+        let body = JSON.parse(response);
+        $('#test').append(`<br>The humidity in ${body.name} is ${body.main.humidity}%`);
+        $('#test').append(`<br>The temperature in Kelvins is ${body.main.temp}.`);
+        $('#test').append(`<br>The current forecast is ${body.weather[0].main} (${body.weather[0].description}).`);
+        $('#city').val("");
+      }, function(error) {
+        $('#test').append(`<br>There was an error processing your request: ${error.message} Please try again.`);
       });
+
+
+      /* API call with jQuery ajax method ---------- */
+      // $.ajax({
+      //   url: `http://api.openweathermap.org/data/2.5/weather?q=${$("#city").val()}&APPID=${process.env.exports.apiKey}`,
+      //   type: 'GET',
+      //   data: {
+      //     format: 'json'
+      //   },
+      //   success: function (response) {
+      //     $('#test').append(`<br>The humidity in ${response.name} is ${response.main.humidity}%`);
+      //     $('#test').append(`<br>The temperature in Kelvins is ${response.main.temp}.`);
+      //     $('#test').append(`<br>The current forecast is ${response.weather[0].main} (${response.weather[0].description}).`);
+      //     $('#city').val("");
+      //   },
+      //   error: function () {
+      //     $('#test').append("<br>There was an error processing your request. Please try again.");
+      //   }
+      // });
+
+
+      /* API call with Javascript's XMLHttpRequest object ---------- */
+      // let request = new XMLHttpRequest();
+      // const url = `http://api.openweathermap.org/data/2.5/weather?q=${$("#city").val()}&APPID=${process.env.exports.apiKey}`;
+
+      // request.onreadystatechange = function() {
+      //   if (this.readyState === 4 && this.status === 200) {
+      //     const response = JSON.parse(this.responseText);
+      //     getElements(response);
+      //   }
+      // }
+
+      // request.open("GET", url, true);
+      // request.send();
+
+      // const getElements = function(response) {
+      //   $('#test').append(`<br>The humidity in ${response.name} is ${response.main.humidity}%`);
+      //   $('#test').append(`<br>The temperature in Kelvins is ${response.main.temp}.`);
+      //   $('#test').append(`<br>The current forecast is ${response.weather[0].main} (${response.weather[0].description}).`);
+      //   $('#city').val("");
+
+      // }
+
+
+      /* API call with jQuery shorthand get() method and promises then() method ---------- */
+      // $.get(`http://api.openweathermap.org/data/2.5/weather?q=${$("#city").val()}&APPID=${process.env.exports.apiKey}`).then(function(response) {
+      //   $('#test').append(`<br>The humidity in ${response.name} is ${response.main.humidity}%`);
+      //   $('#test').append(`<br>The temperature in Kelvins is ${response.main.temp}.`);
+      //   $('#test').append(`<br>The current forecast is ${response.weather[0].main} (${response.weather[0].description}).`);
+      //   $('#city').val("");
+      // }).fail(function(error) {
+      //   $('#test').append(`<br>There was an error processing your request: ${error.responseText} Please try again.`);
+      // });
+
+
+      /* API call with Javascript's XMLHttpRequest object and Javascripts Promise object ---------- */
+    //   let promise = new Promise(function(resolve, reject) {
+    //     let request = new XMLHttpRequest();
+    //     let url = `http://api.openweathermap.org/data/2.5/weather?q=${$("#city").val()}&APPID=${process.env.exports.apiKey}`;
+  
+    //     request.onload = function() {
+    //       if (this.status === 200) {
+    //         resolve(request.response);
+    //       } else {
+    //         reject(Error(request.statusText));
+    //       }
+    //     }
+    //     request.open("GET", url, true);
+    //     request.send();
+    //   });
+
+    //   promise.then(function(response) {
+    //     let body = JSON.parse(response);
+    //     $('#test').append(`<br>The humidity in ${body.name} is ${body.main.humidity}%`);
+    //     $('#test').append(`<br>The temperature in Kelvins is ${body.main.temp}.`);
+    //     $('#test').append(`<br>The current forecast is ${body.weather[0].main} (${body.weather[0].description}).`);
+    //     $('#city').val("");
+
+    //   }, function(error) {
+    //     $('#test').append(`<br>There was an error processing your request: ${error.message} Please try again.`);
+    //   });
+    // }
+
     }
   });
-
 });
