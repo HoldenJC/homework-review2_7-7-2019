bb6b5866e0da4197d2894e47c9d0909e91e53335
HoldenJC <holdenj_c@yahoo.com>
2019-07-07 21:53:50 -0700

    add open weather API calls, basic UI and user input field for city

--- src/main.js
+++ src/main.js
@@ -6,8 +6,31 @@ import { Example } from './backend-code';
 
 let example = new Example();
 
-$(document).ready(function() {
-  $("#test").append(example.name);
+$(document).ready(function () {
+  $("#test").append(`<br>${example.name}`);
   example.exampleMethod();
-  $("#test").append(example.name);
+  $("#test").append(`<br>${example.name}`);
+
+  $("#city").keyup(function (event) {
+    if (event.keyCode === 13) {
+
+      $.ajax({
+        url: `http://api.openweathermap.org/data/2.5/weather?q=${$("#city").val()}&APPID=${process.env.exports.apiKey}`,
+        type: 'GET',
+        data: {
+          format: 'json'
+        },
+        success: function (response) {
+          $('#test').append(`<br>The humidity in ${response.name} is ${response.main.humidity}%`);
+          $('#test').append(`<br>The temperature in Kelvins is ${response.main.temp}.`);
+          $('#test').append(`<br>The current forecast is ${response.weather[0].main} (${response.weather[0].description}).`);
+          $('#city').val("");
+        },
+        error: function () {
+          $('#test').append("<br>There was an error processing your request. Please try again.");
+        }
+      });
+    }
+  });
+
 });
